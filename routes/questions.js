319a7dd0fc7be838977789a0b1a7a9b9ab1da49c
aera <allain_era@yahoo.com>
2017-08-01 11:39:54 -0700

    edit routes and made partials and questions views

--- routes/questions.js
+++ routes/questions.js
@@ -2,12 +2,16 @@ const express = require('express');
 const router = express.Router();
 const {Question} = require('../models');
 
-/* GET users listing. */
+// questions#index PATH: /questions METHOD: GET
 router.get('/', function(req, res, next) {
   Question
     .all()
     .then(questions => {
-      res.send(questions);
+      // To pass a variable to a template, pass
+      // an object as a second argument to res.render.
+      // All the properties of that object will be available
+      // as local variables inside of the template.
+      res.render('questions/index', {questions});
     })
 });
 
